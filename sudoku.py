8154a247a4a04527eec2233841e5117e9511fe0f
Mario Tornería R <mh.torneria@gmail.com>
2022-03-27 01:12:34 -0300

    Variables almacenadas en cuadrantes según su letra inicial. Requiero almacenar variables en filas y columnas correspondientes a la matriz del sudoku, para evaluar el valor numerico de cada variable

--- sudoku.py
+++ sudoku.py
@@ -2,7 +2,6 @@ Letras_a = ["A","B","C","D","E","F","G","H","I"]
 Letras_b = ["A","B","C","D","E","F","G","H","I"]
 Letras_Merge = []
 count = 8
-
 while count > -1:
     for i in range(len(Letras_a)):
         Letras_Merge.append(Letras_a[count]+Letras_b[i])
@@ -19,11 +18,27 @@ QuadG=[]
 QuadH=[]
 QuadI=[]
 
+rango = 9 #Variable para aumentar en múltiplos de 9 el i
+for i in range(0,9): #Distribución de las letras en cuadrantes según su letra inicial
+    i=i%9
+    QuadA.append(Letras_Merge[i])
+    QuadB.append(Letras_Merge[i+(rango*1)])
+    QuadC.append(Letras_Merge[i+(rango*2)])
+    QuadD.append(Letras_Merge[i+(rango*3)])
+    QuadE.append(Letras_Merge[i+(rango*4)])
+    QuadF.append(Letras_Merge[i+(rango*5)])
+    QuadG.append(Letras_Merge[i+(rango*6)])
+    QuadH.append(Letras_Merge[i+(rango*7)])
+    QuadI.append(Letras_Merge[i+(rango*8)])
+
 
 #Las siguientes lineas son prescindibles a futuro, sólo existen para corroborar la función del programa
 
 #for i in range(len(Letras_Merge)):
-#    print(Letras_Merge[i])
+#    print(Letras_Merge[i]
 
-for i in range(len(Letras_a)):
-    QuadA.append(Letras_Merge[i])
+
+
+
+
+
