d44341a8086138685d57f0322385316c1806119b
Mario Tornería R <mh.torneria@gmail.com>
2022-03-26 23:52:28 -0300

    Las 81 variables de los 81 cuadros del sudoku, calculadas

--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,13 @@
 Letras_a = ["A","B","C","D","E","F","G","H","I"]
 Letras_b = ["A","B","C","D","E","F","G","H","I"]
 Letras_Merge = []
-ciclo = len(Letras_a)
-while ciclo == 0:
-    ciclo=ciclo-1
+count = 8
+
+while count > -1:
     for i in range(len(Letras_a)):
-        Letras_Merge.append(Letras_a[i]+Letras_b[ciclo])
-    for i in range(len(Letras_Merge)):
-        print(Letras_Merge[i])
+        Letras_Merge.append(Letras_a[count]+Letras_b[i])
+    count=count-1
+
+Letras_Merge.sort()
+for i in range(len(Letras_Merge)):
+    print(Letras_Merge[i])
