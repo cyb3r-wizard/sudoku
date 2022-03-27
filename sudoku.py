7918d86c1f797d075573a101b139d9e0d87afca6
Mario Tornería R <mh.torneria@gmail.com>
2022-03-26 21:54:25 -0300

    creación del listado de  81 variables correspondientes a los 81 cuadros de un sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,10 @@
 Letras_a = ["A","B","C","D","E","F","G","H","I"]
 Letras_b = ["A","B","C","D","E","F","G","H","I"]
 Letras_Merge = []
-for i in range(len(Letras_a)):
-    Letras_Merge.append(Letras_a[i]+Letras_b[i])
-for i in range(len(Letras_Merge)):
-    print(Letras_Merge[i])
+ciclo = len(Letras_a)
+while ciclo == 0:
+    ciclo=ciclo-1
+    for i in range(len(Letras_a)):
+        Letras_Merge.append(Letras_a[i]+Letras_b[ciclo])
+    for i in range(len(Letras_Merge)):
+        print(Letras_Merge[i])
