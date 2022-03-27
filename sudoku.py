3c7ab4334408ec00118973174c42ba46791add03
Mario Tornería R <mh.torneria@gmail.com>
2022-03-26 21:31:44 -0300

    prototipo 1, generando nombres para la matriz numérica

---
+++ sudoku.py
@@ -0,0 +1,7 @@
+Letras_a = ["A","B","C","D","E","F","G","H","I"]
+Letras_b = ["A","B","C","D","E","F","G","H","I"]
+Letras_Merge = []
+for i in range(len(Letras_a)):
+    Letras_Merge.append(Letras_a[i]+Letras_b[i])
+for i in range(len(Letras_Merge)):
+    print(Letras_Merge[i])
